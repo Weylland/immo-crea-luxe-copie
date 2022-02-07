5c53e02fba4a59c358d63abc7ba9643bc804d370
Weylland <38096642+Weylland@users.noreply.github.com>
2022-02-07 09:32:47 +0100

    Ajout fusion
    J'ai ajouter la fusion des dossiers

--- js/index.js
+++ js/index.js
@@ -5,6 +5,24 @@ const burgerMenu = document.getElementById("burgerMenu");
 const burgerMenuI = document.getElementById("burgerMenuI");
 const scrollToTop = document.getElementById("scrollToTop");
 
+
+//  CARROUSEL 
+
+let slide = new Array(
+  "asset/img/img-1-carrousel.png",
+  "asset/img/carrousel-2.png",
+  "asset/img/carrousel-3.png"
+);
+
+let numero = 0;
+
+function ChangeSlide(sens) {
+  numero = numero + sens;
+  if (numero < 0) numero = slide.length - 1;
+  if (numero > slide.length - 1) numero = 0;
+  document.getElementById("slide").src = slide[numero];
+}
+
 // LARGE NAV ANIMATION
 
 window.onscroll = function () {
